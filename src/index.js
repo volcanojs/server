8bf3f08f99e550369f8f8c1fc1db411a671c8ff0
Kang Cheng <me@chengkang.pw>
2018-02-13 22:56:13 +0000

    added `Vue` & console APIs
    1. get projects
    2. create project
    3. delete project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const MongoClient = require('mongodb').MongoClient
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+const ObjectID = mongodb.ObjectID
 const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -30,31 +32,44 @@ MongoClient.connect(url, (err, client) => {
   const service = require('./service')(db)
 
   app.get('/', (req, res) => {
-    coll.find({}).toArray()
-      .then(projects => {
-        res.render('pages/home', {
-          projects,
-        })
-      })
-      .catch(error => res.status(500).json({ error }))
-  })
-
-  app.post('/api/console/project', jsonParser, (req, res) => {
-    try {
-      const { name } = req.body
-      const curTime = (new Date()).getTime()
-      coll.insertOne({ name, createdAt: curTime, updatedAt: curTime })
-        .then(doc => res.status(200).json(doc))
-        .catch(error => res.status(400).json({ error }))
-    } catch (error) {
-      return res.status(400).json({ error })
-    }
-    // coll.insertOne( { x: 1 } )
+    res.render('pages/home')
   })
 
   io.on('connection', function (socket) {
+    console.log('Welcome', socket.id)
     // TODO: handle operations on root
     require('./event')({ io, socket, service })
+
+    socket.on('/api/console/projects', async (_, callback) => {
+      console.log('Hi')
+      try {
+        const projects = await coll.find({}).sort( { createdAt: -1 } ).toArray()
+        console.log(projects)
+        callback({ data: projects })
+      } catch (error) {
+        callback({ error })
+      }
+    })
+    socket.on('post /api/console/project', async ({ name }, callback) => {
+      try {
+        const curTime = (new Date()).getTime()
+        const result = await coll.insertOne({ name, createdAt: curTime, updatedAt: curTime })
+        socket.emit('/api/console/project-new', result.ops[0])
+        callback({})
+      } catch (error) {
+        callback({ error })
+      }
+    })
+    socket.on('delete /api/console/project', async ({ _id }, callback) => {
+      try {
+        const curTime = (new Date()).getTime()
+        const result = await coll.deleteOne({ _id: ObjectID(_id) })
+        console.log(result)
+        callback({ error: result.deletedCount > 0 ? null : 'Failed to delete.' })
+      } catch (error) {
+        callback({ error })
+      }
+    })
   })
 })
 
